f79366af2835a15f26e420abfe813c678d1d9c82
Kalam <Kalam@DESKTOP-2URI2JC>
2022-03-25 11:35:41 +0600

    popular section list view text container layout

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -67,7 +67,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
         SizedBox(height: Dimensions.height20),
         Container(
           margin: EdgeInsets.only(
-              left: Dimensions.width30, bottom: Dimensions.height10),
+              left: Dimensions.width30, bottom: Dimensions.height20),
           child: Row(
             crossAxisAlignment: CrossAxisAlignment.end,
             children: [
@@ -95,54 +95,90 @@ class _FoodPageBodyState extends State<FoodPageBody> {
           ),
         ),
         //List of food and image
-        Container(
-          height: 900,
-          child: ListView.builder(
-            physics: NeverScrollableScrollPhysics(),
-            //shrinkWrap: true,
-            itemCount: 10,
-            itemBuilder: (context, index) {
-              return Container(
-                margin: EdgeInsets.only(
-                  left: Dimensions.width20,
-                  right: Dimensions.width20,
-                  bottom: Dimensions.height10,
-                ),
-                child: Row(
-                  children: [
-                    //image section
-                    Container(
-                      height: 120,
-                      width: 120,
+        ListView.builder(
+          physics: NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return Container(
+              margin: EdgeInsets.only(
+                left: Dimensions.width20,
+                right: Dimensions.width20,
+                bottom: Dimensions.height10,
+              ),
+              child: Row(
+                children: [
+                  //image section
+                  Container(
+                    height: Dimensions.listViewImgSize,
+                    width: Dimensions.listViewImgSize,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(
+                        Dimensions.radius20,
+                      ),
+                      color: Colors.white38,
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage("assets/image/food0.png"),
+                      ),
+                    ),
+                  ),
+                  //txt section
+                  Expanded(
+                    child: Container(
+                      height: Dimensions.listViewTextContSize,
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(
-                          Dimensions.radius20,
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(Dimensions.radius20),
+                          bottomRight: Radius.circular(Dimensions.radius20),
                         ),
-                        color: Colors.white38,
-                        image: DecorationImage(
-                          fit: BoxFit.cover,
-                          image: AssetImage("assets/image/food0.png"),
+                        color: Colors.white,
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                            left: Dimensions.width10,
+                            right: Dimensions.width10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            BigText(text: 'Nutritious fruit meal in China'),
+                            SizedBox(
+                              height: Dimensions.height10,
+                            ),
+                            SmallText(text: 'With chinese characteristics'),
+                            SizedBox(
+                              height: Dimensions.height10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                IconAndTextWidget(
+                                  icon: Icons.circle_sharp,
+                                  text: 'Normal',
+                                  iconColor: AppColors.iconColor1,
+                                ),
+                                IconAndTextWidget(
+                                  icon: Icons.location_on,
+                                  text: '1.7km',
+                                  iconColor: AppColors.mainColor,
+                                ),
+                                IconAndTextWidget(
+                                  icon: Icons.access_time_rounded,
+                                  text: '32min',
+                                  iconColor: AppColors.iconColor2,
+                                ),
+                              ],
+                            )
+                          ],
                         ),
                       ),
                     ),
-                    //txt section
-                    Expanded(
-                      child: Container(
-                        height: 100,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                            topRight: Radius.circular(Dimensions.radius20),
-                            bottomRight: Radius.circular(Dimensions.radius20),
-                          ),
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            },
-          ),
+                  ),
+                ],
+              ),
+            );
+          },
         ),
       ],
     );
--- lib/utils/dimensions.dart
+++ lib/utils/dimensions.dart
@@ -7,6 +7,7 @@ class Dimensions {
   static double pageView = screenHeight / 2.28;
   static double pageViewContainer = screenHeight / 3.2;
   static double pageViewTextContainer = screenHeight / 6.4;
+
 //dynamic height padding and margin
   static double height5 = screenHeight / 128;
   static double height10 = screenHeight / 64;
@@ -14,6 +15,7 @@ class Dimensions {
   static double height20 = screenHeight / 32;
   static double height30 = screenHeight / 21.33;
   static double height40 = screenHeight / 16;
+
 //dynamic width padding and margin
   static double width5 = screenHeight / 128;
   static double width10 = screenHeight / 64;
@@ -22,11 +24,18 @@ class Dimensions {
   static double width30 = screenHeight / 21.33;
   static double width40 = screenHeight / 16;
 
+  //font size
   static double font20 = screenHeight / 32;
+
   //radius
   static double radius15 = screenHeight / 42.67;
   static double radius20 = screenHeight / 32;
   static double radius30 = screenHeight / 21.33;
+
   //Icon Size
   static double iconSize24 = screenHeight / 26.67;
+
+  //list view size
+  static double listViewImgSize = screenWidth / 3.6;
+  static double listViewTextContSize = screenWidth / 3.6;
 }
